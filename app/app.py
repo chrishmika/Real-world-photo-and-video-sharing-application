0e8b23a413aaab5b6b5a4824899efcd1bb15898d
chrishmika <shehanchrishmika123@gmail.com>
2026-04-15 03:15:14 +0530

    update

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-
+from app.schemas import PostCreate
 
 server = FastAPI()
 
@@ -13,13 +13,21 @@ text_post = {
 }
 
 @server.get("/posts")
-def get_al_posts():
-    return text_post
-    
-    
+def get_all_posts(limit:int = None):
+    if(limit):
+        return list(text_post.values())[:limit]
+    return text_post.values()
+
 @server.get("/posts/{id}")
 def get_post(id:int):
     if id not in text_post:
         raise HTTPException(status_code=404, detail="Post not found")
 
     return text_post.get(id)
+
+
+@server.post("/posts")
+def create_post(post:PostCreate):
+    post = {"title": post.title, "post": post.content}
+    text_post[max(text_post.keys())+1] = post
+    return post
